a9117a08e10c3bcbc24ea3492962996ec51ee2ef
Paul Jensen <paulbjensen@gmail.com>
2020-08-25 20:13:20 +0100

    got the example to 98 lines of code - wow

--- game-three/main.lua
+++ game-three/main.lua
@@ -1,5 +1,3 @@
--- Image files for grass and dirt
--- the image files are 64 pixels wide and 32 pixels high
 local dirt = love.graphics.newImage("dirt2.png")
 local grass = love.graphics.newImage("grass4.png")
 local roadltr = love.graphics.newImage("road-left-to-right.png")
@@ -8,30 +6,19 @@ local crossroad = love.graphics.newImage("crossroad.png")
 local signpost = love.graphics.newImage("signpost.png")
 love.window.setTitle("Shop Tycoon")
 love.window.setMode(1200, 800, {resizable = true})
-
 local tileset = {grass, dirt, roadltr, roadrtl, crossroad, signpost}
-
--- window dimensions
 local window_width, window_height = love.graphics.getDimensions()
-
--- blocks, calculated based on an image's width and height
 local block_width = grass:getWidth()
 local block_height = grass:getHeight()
-
 local block_dimension = -1 * block_width
-
--- the grid of tiles that we render to the screen
 local grid_size = 20
 local grid_x = (window_width / 2) - (block_width / 2)
-local grid_y = (window_height / 2) - (block_height / 2) -- I think this is slightly knocking if off center on zoom
-
+local grid_y = (window_height / 2) - (block_height / 2)
 local camera_offset = {}
 local zoom_level = 1
 camera_offset.x = 0
 camera_offset.y = 0
 
--- here we populate the grid to indicate what kind of tile we want to load at
--- which coordinate
 local grid = {}
 for x = 1, grid_size do
 	grid[x] = {}
@@ -49,18 +36,12 @@ for x = 1, grid_size do
 		if (x == 4 and y == 8) then
 			grid[x][y] = {5}
 		end
+		if (x >=2 and x <= 3 and y >= 3 and y <= 5) then 
+			grid[x][y] = {2, 6}
+		end
 	end
 end
 
--- This draws a 2x3 grid of dirt
-grid[2][3] = {2, 6}
-grid[2][4] = {2, 6}
-grid[2][5] = {2, 6}
-grid[3][3] = {2, 6}
-grid[3][4] = {2, 6}
-grid[3][5] = {2, 6}
-
--- this draws the tile image at the grid coordinate
 function draw_tile(image, x, y)
 	love.graphics.draw(
 		image,
@@ -97,7 +78,7 @@ function love.update()
 	end
 
 	if love.keyboard.isDown("0") then
-		zoom_level = 1.0
+		zoom_level = 1
 	end
 end
 
@@ -108,9 +89,8 @@ function love.draw()
 		for y = 1, grid_size do
 			local tiles = grid[x][y]
 			for z = 1, #tiles do
-				local tile = tileset[tiles[z]]
-				if (tile) then
-					draw_tile(tile, x, y)
+				if (tileset[tiles[z]]) then
+					draw_tile(tileset[tiles[z]], x, y)
 				end
 			end
 		end
